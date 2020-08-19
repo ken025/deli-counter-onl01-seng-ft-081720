bc8b7fac3dce2b06b262bc1d2d1a9e200bdef316
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-19 04:40:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,14 @@
 katz_deli = []
 
-def line(array) # this was the one I figured out
+def line(array) 
+  
   if array.length >= 1
     nuarray = []
     counter = 1 
     array.each do |name|
       nuarray.push("#{counter}. #{name}")
       counter += 1 
+      
     end 
     puts "The line is currently: #{nuarray.join(" ")}"
   else
@@ -14,5 +16,3 @@ def line(array) # this was the one I figured out
   end
 end
 
-end 
-end 
